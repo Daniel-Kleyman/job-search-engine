ce4d3848c192d83291fd0c1c8890ad9dea6dfd8e
Daniel Kleyman <denny_klm1@yahoo.com>
2024-08-19 22:55:06 +0300

    updated service files

--- job-search-ind/src/main/java/com/danielkleyman/jobsearchind/service/ExtractJobDetails.java
+++ job-search-ind/src/main/java/com/danielkleyman/jobsearchind/service/ExtractJobDetails.java
@@ -4,21 +4,16 @@ package com.danielkleyman.jobsearchind.service;
 import com.danielkleyman.jobsearchapi.service.AIService;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
-import java.util.*;
-import java.util.concurrent.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import static com.danielkleyman.jobsearchind.service.IndService.LOGGER;
 
 
 @Component
@@ -66,12 +61,14 @@ public class ExtractJobDetails {
                         String city = jobNode.path("jobLocationCity").asText();
                         String url = jobNode.path("link").asText();
                         String jobDescription = jobNode.path("snippet").asText();
+                        String jobKey = jobNode.path("jobkey").asText();
 
                         System.out.println("Job Title: " + jobTitle);
                         System.out.println("Company: " + companyName);
                         System.out.println("City: " + city);
                         System.out.println("URL: " + url);
                         System.out.println("Description: " + jobDescription);
+                        System.out.println("Job Key: " + jobKey);
                         System.out.println("-------------------------------");
                     }
                 }
@@ -83,196 +80,53 @@ public class ExtractJobDetails {
             System.out.println("No embedded data found.");
         }
     }
-//        System.out.println("Waiting for page to load...");
+//    private void extractJobDetails(WebDriver driver, WebDriverWait wait, String jobKey) {
+//        // Navigate to the full job details page using the jobKey
+//        String url = "https://www.indeed.com/m/basecamp/viewjob?viewtype=embedded&jk=" + jobKey;
+//        driver.get(url);
 //
-//        try {
-//            System.out.println("Searching for job container");
-//            // Wait until the job container is visible
-//            WebElement jobContainer = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//ul[contains(@class, 'jobs-search__results-list')]")));
-//            System.out.println("Job container is found");
-//            // Find all job cards on the page
-//            List<WebElement> jobCards = driver.findElements(By.xpath("//div[contains(@class, 'job-search-card')]"));
-//            if (jobCards.isEmpty()) {
-//                System.err.println("No job cards found.");
-//                return;
+//        // Wait for the page to load and the _initialData to be present
+//        wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("body")));
+//
+//        // Get the page source
+//        String pageSource = driver.getPageSource();
+//
+//        // Define the regex pattern to find the embedded JSON data
+//        String regexPattern = "_initialData=(\\{.+?\\});";
+//        Pattern pattern = Pattern.compile(regexPattern);
+//        Matcher matcher = pattern.matcher(pageSource);
+//
+//        if (matcher.find()) {
+//            String jsonString = matcher.group(1);
+//
+//            // Parse the JSON data
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            try {
+//                JsonNode rootNode = objectMapper.readTree(jsonString);
+//                JsonNode jobInfoWrapperNode = rootNode.path("jobInfoWrapperModel");
+//                JsonNode jobInfoModelNode = jobInfoWrapperNode.path("jobInfoModel");
+//
+//                // Extract and print job details
+//                String jobTitle = jobInfoModelNode.path("title").asText();
+//                String companyName = jobInfoModelNode.path("company").asText();
+//                String city = jobInfoModelNode.path("location").asText();
+//                String url = jobInfoModelNode.path("url").asText();
+//                String jobDescription = jobInfoModelNode.path("description").asText();
+//
+//                System.out.println("Job Title: " + jobTitle);
+//                System.out.println("Company: " + companyName);
+//                System.out.println("City: " + city);
+//                System.out.println("URL: " + url);
+//                System.out.println("Description: " + jobDescription);
+//                System.out.println("-------------------------------");
+//
+//            } catch (Exception e) {
+//                e.printStackTrace();
 //            }
-//            System.out.println("Job cards are found");
-//            jobCardsParsing(driver, wait, jobDetails, jobCards);
-//
-//        } catch (TimeoutException e) {
-//            System.err.println("Timeout while waiting for job container: " + e.getMessage());
-//        } catch (Exception e) {
-//            System.err.println("Unexpected error: " + e.getMessage());
+//        } else {
+//            System.out.println("No embedded data found.");
 //        }
-//        System.out.println("Jobs visible: " + jobsVisibleOnPage);
-//        jobsVisibleOnPage = 0;
-    //   }
-
-    private void jobCardsParsing(WebDriver driver, WebDriverWait wait, Map<String, List<String>> jobDetails, List<WebElement> jobCards) throws TimeoutException {
-        for (int i = 0; i < jobCards.size(); i++) {
-            int timeout = 15 * 10000;
-            final int index = i;
-            boolean stopParsing = executeWithTimeout(() -> singleCardParsing(jobCards, index, driver, wait, jobDetails), timeout);
-            if (!stopParsing) {
-                continue;
-            }
-        }
-    }
-
-    private boolean singleCardParsing(List<WebElement> jobCards, int i, WebDriver driver, WebDriverWait wait, Map<String, List<String>> jobDetails) {
-        jobsVisibleOnPage++;
-        WebElement jobCard = jobCards.get(i);
-        System.out.println("get card number: " + i);
-        List<String> url = new ArrayList<>();
-        List<String> details = new ArrayList<>();
-        try {
-            // Extract job title and URL
-            boolean extractionTitleAndUrl = executeWithTimeout(() -> extractTitleAndUrl(jobCard, details, url), 20000);
-            if (!extractionTitleAndUrl) {
-                return false; // Skip to the next job card if extraction failed
-            }
-            String jobUrl = url.get(0);
-            // Skip processing if URL has already been processed
-            int positionIndex = jobUrl.indexOf("position");
-            String extractedPart = jobUrl.substring(0, positionIndex + "position".length());
-            if (IndService.alreadyAdded.contains(extractedPart)) {
-                System.out.println("URL already been added: " + jobUrl);
-                return false; // Continue with the next job card
-            }
-            showExpandedContent(wait, i, jobCard, jobCards);
-            boolean extractionExpandedContent = extractExpandedContent(driver, details);
-            if (!extractionExpandedContent) {
-                return false; // Skip to the next job card if extraction failed
-            }
-            // Extract company name
-            extractCompanyName(details, wait);
-            // Extract city
-            extractCity(details, wait);
-
-            // Add job details to the map
-            jobDetails.putIfAbsent(jobUrl, details);
-            IndService.alreadyAdded.add(extractedPart);
-        } catch (Exception e) {
-            System.err.println("Unexpected error extracting details from job card: " + e.getMessage());
-        }
-        return true;
-    }
-
-    private boolean executeWithTimeout(Callable<Boolean> callable, long timeoutMillis) throws TimeoutException, TimeoutException {
-        int maxRetries = 3;
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-
-        for (int attempt = 1; attempt <= maxRetries; attempt++) {
-            Future<Boolean> future = executor.submit(callable);
-
-            try {
-                return future.get(timeoutMillis, TimeUnit.MILLISECONDS);
-            } catch (TimeoutException e) {
-                if (attempt == maxRetries) {
-                    // If the last attempt fails, propagate the TimeoutException
-                    throw e;
-                }
-                System.out.println("Retry attempt " + attempt + " failed due to timeout, retrying...");
-            } catch (InterruptedException | ExecutionException e) {
-                LOGGER.severe("Exception while executing callable: " + e.getMessage());
-                return false;
-            } finally {
-                future.cancel(true); // Optionally cancel the future if it's still running
-            }
-        }
-
-        executor.shutdownNow(); // Ensure that the executor is shut down after all retries
-        return false;
-    }
-
-    private void extractCity(List<String> details, WebDriverWait wait) {
-        try {
-            WebElement cityElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.topcard__flavor.topcard__flavor--bullet")));
-            String city = cityElement.getText().trim();
-            details.add(city);
-        } catch (NoSuchElementException e) {
-            System.err.println("City element not found: " + e.getMessage());
-            details.add("City not available");
-        }
-    }
-
-    private void extractCompanyName(List<String> details, WebDriverWait wait) {
-        try {
-            WebElement companyElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a.topcard__org-name-link")));
-            String companyName = companyElement.getText();
-            details.add(companyName);
-        } catch (NoSuchElementException e) {
-            System.err.println("Company name element not found: " + e.getMessage());
-            details.add("Company name not available");
-        }
-    }
-
-    private boolean extractExpandedContent(WebDriver driver, List<String> details) {
-        // Use a shorter wait for the expanded content
-        WebDriverWait shortWait = new WebDriverWait(driver, Duration.ofSeconds(1));
-        WebElement expandedContent = null;
-        expandedContent = shortWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.show-more-less-html__markup")));
-        String extendedText = expandedContent.getText();
-        if (!filterDescription(extendedText)) {
-            System.out.println("Extended text excluded for job: " + details.get(0));
-            return false; // Skip this job card if the extended text does not match filter criteria
-        }
-        int aiResponse = aiService.getResponse(extendedText);
-        System.out.println(" " + details.get(0) + " gpt score = " + aiResponse);
-        if (aiResponse < 21) {
-            //   System.out.println("text for job title:  " + details.get(0) + " excluded by ai = " + aiResponse);
-            return false; // Skip this job card if the extended text does not match filter criteria
-        }
-        details.add(extendedText);
-        //                    System.out.println("Extended text added: " + extendedText);
-        return true;
-    }
-
-    private void showExpandedContent(WebDriverWait wait, int i, WebElement jobCard, List<WebElement> jobCards) {
-        boolean showMoreVisible = false;
-        while (!showMoreVisible) {
-            // Click on the job card to expand it
-            jobCard.click();
-            try {
-                // Try to find and click the "Show more" button
-                WebElement showMoreButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("button.show-more-less-html__button")));
-                showMoreButton.click();
-                showMoreVisible = true; // Exit loop if the button was clicked
-                System.out.println("Clicked 'Show more' button.");
-            } catch (Exception e) {
-                // If the "Show more" button is not found, collapse the previous job card if possible
-                if (i > 0) {
-                    WebElement previousJobCard = jobCards.get(i - 1);
-                    previousJobCard.click(); // Collapse the previous job card
-                    System.out.println("Collapsed previous job card.");
-                }
-                // Retry the current job card by clicking it again
-                jobCard.click();
-            }
-        }
-    }
-
-    private boolean extractTitleAndUrl(WebElement jobCard, List<String> details, List<String> url) {
-        String title = "";
-        try {
-            WebElement titleElement = jobCard.findElement(By.xpath(".//h3[contains(@class, 'base-search-card__title')]"));
-            title = titleElement.getText();
-            if (!filterTitle(title.toLowerCase())) {
-                System.out.println("Job title excluded: " + title);
-                return false; // Skip this job card if title matches filter criteria
-            }
-            details.add(title);
-
-            WebElement urlElement = jobCard.findElement(By.cssSelector("a.base-card__full-link"));
-            String urlAddress = urlElement.getAttribute("href");
-            url.add(urlAddress);
-        } catch (NoSuchElementException e) {
-            System.err.println("Job title or URL element not found in a job card.");
-            return false; // Skip this job card if title or URL is missing
-        }
-        return true;
-    }
-
+//    }
     private static boolean filterTitle(String jobTitle) {
         Set<String> excludeKeywords = Set.of(
                 "senior", "lead", "leader", "devops", "manager", "qa", "mechanical", "infrastructure", "integration", "civil",
