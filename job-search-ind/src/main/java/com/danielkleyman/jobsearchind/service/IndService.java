1ed495dff1d201a5340c1b3b52a95835d8888305
Daniel Kleyman <denny_klm1@yahoo.com>
2024-08-21 16:55:44 +0300

    job-search-ind refactoring

--- job-search-ind/src/main/java/com/danielkleyman/jobsearchind/service/IndService.java
+++ job-search-ind/src/main/java/com/danielkleyman/jobsearchind/service/IndService.java
@@ -25,10 +25,6 @@ public class IndService {
     public static final String CHROME_DRIVER_PATH = System.getenv("CHROME_DRIVER_PATH");
     private static final String WEBSITE_NAME = "Indeed";
     private final String URL = "https://il.indeed.com/jobs?q=&l=israel&fromage=1&vjk=087662bdbb912e05";
-//    private final String URL1 = "https://il.indeed.com/jobs?q=engineer&l=israel&fromage=1&vjk=1305588085fc99a8";
-//    private final String URL2 = "https://il.indeed.com/jobs?q=developer&l=israel&fromage=1&vjk=3ad75f0141d7c30a";
-//    private final String URL3 = "https://il.indeed.com/jobs?q=%D7%9E%D7%A4%D7%AA%D7%97&l=israel&fromage=1&vjk=810b903b668ad339";
-//    private final String URL4 = "https://il.indeed.com/jobs?q=%D7%9E%D7%AA%D7%9B%D7%A0%D7%AA&l=israel&fromage=1&vjk=0fc87a153af906f9";
     private final Map<String, List<String>> JOB_DETAILS = new LinkedHashMap<>();
     private final ExtractJobDetails extractJobDetails; // Injected dependency
     public static List<String> urlAlreadyAdded = new ArrayList<>();
@@ -59,10 +55,9 @@ public class IndService {
     }
 
     public void getResults(WebDriver driver, WebDriverWait wait) {
-        // Scrolling scroller = new Scrolling(driver);
         jobCount = 0;
         long startTime = System.currentTimeMillis();
-        openPage(driver, wait, URL);
+        driver.get(URL);
         try {
             while (isNextPageButtonVisible(driver, wait)) {
                 Thread.sleep(randomTimeoutCalculation(4000, 8000));
@@ -100,10 +95,8 @@ public class IndService {
         try {
             // Wait until the "Next Page" button is clickable
             WebElement nextPageButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[data-testid='pagination-page-next']")));
-
             // Click on the "Next Page" button
             nextPageButton.click();
-
             System.out.println("Navigated to the next page.");
         } catch (Exception e) {
             System.err.println("Failed to click on the Next Page button: " + e.getMessage());
@@ -116,55 +109,15 @@ public class IndService {
         return min + random.nextInt((int) (max - min + 1));
     }
 
-    private void openPage(WebDriver driver, WebDriverWait wait, String url) {
-        boolean reload = true;
-        //      while (reload) {
-        driver.get(url);
-//            try {
-//                WebElement jobCounter = wait.until(ExpectedConditions.visibilityOfElementLocated(
-//                        By.cssSelector(".results-context-header__job-count")));
-//
-//                if (jobCounter != null) {
-//                    LOGGER.info("proceeding");
-//                    reload = false;
-//                }
-//
-//            } catch (TimeoutException e) {
-//                try {
-//                    WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"main-content\"]/section[1]/h1/strong")));
-//                    try {
-//                        LOGGER.info("waiting time before reloading page");
-//                        Thread.sleep(900000);
-//                    } catch (InterruptedException ex) {
-//                        Thread.currentThread().interrupt();
-//                        LOGGER.severe("Interrupted while waiting: " + ex.getMessage());
-//                    }
-//                } catch (TimeoutException ignored) {
-//                }
-//                LOGGER.info("reloading page");
-//            }
-//            try {
-//                Thread.sleep(2000);
-//            } catch (InterruptedException ex) {
-//                Thread.currentThread().interrupt();
-//                LOGGER.severe("Interrupted while waiting: " + ex.getMessage());
-//            }
-//        }
-    }
-
     private WebDriver initializeWebDriver() {
         if (CHROME_DRIVER_PATH == null) {
             throw new IllegalStateException("CHROME_DRIVER_PATH environment variable not set");
         }
-
         System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_PATH);
-
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--incognito");
-        // options.addArguments("--headless");
         WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-
         return driver;
     }
 }
